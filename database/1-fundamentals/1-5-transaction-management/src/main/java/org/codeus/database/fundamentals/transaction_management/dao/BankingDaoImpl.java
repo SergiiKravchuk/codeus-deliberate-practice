60fc03168bf57853cff1bfde09acb40bbf846cb1
ysavonenko.appwell <ysavonenko.appwell@ancestry.com>
2025-03-31 23:20:00 +0300

    Add detailed doc to BankingDaoImpl

--- database/1-fundamentals/1-5-transaction-management/src/main/java/org/codeus/database/fundamentals/transaction_management/dao/BankingDaoImpl.java
+++ database/1-fundamentals/1-5-transaction-management/src/main/java/org/codeus/database/fundamentals/transaction_management/dao/BankingDaoImpl.java
@@ -21,8 +21,20 @@ public class BankingDaoImpl implements BankingDao {
 
     /**
      * Creates a new account for a customer.
+     * <p>
+     * This method performs the following steps:
+     * <ul>
+     * <li>Starts a transaction by setting auto-commit to false.</li>
+     * <li>Checks if the customer exists in the <code>customers</code> table. If the customer does not exist, throws {@link CustomerNotFoundException}.</li>
+     * <li>Inserts a new record into the <code>accounts</code> table with the provided account details.</li>
+     * <li>Retrieves the generated account ID and sets it to the provided {@code account} object.</li>
+     * <li>Commits the transaction.</li>
+     * <li>If any step fails, rolls back the transaction. If the rollback fails, throws {@link DaoOperationException}.</li>
+     * </ul>
      *
      * @param account the account to be created
+     * @throws CustomerNotFoundException if the customer with the specified ID does not exist
+     * @throws DaoOperationException     if there is an error during the transaction or rollback
      */
     @Override
     public void createAccount(Account account) {
@@ -30,13 +42,29 @@ public class BankingDaoImpl implements BankingDao {
     }
 
     /**
-     * Safely withdraws money from an account using READ COMMITTED isolation.
+     * Withdraws money from an account using READ COMMITTED isolation level.
+     * <p>
+     * This method performs the following steps:
+     * <ul>
+     * <li>Validates if the input <code>amount</code> is less than or equal to 0. If true, throws {@link IllegalArgumentException}.</li>
+     * <li>Starts a transaction by setting auto-commit to false.</li>
+     * <li>Explicitly sets transaction isolation level to READ COMMITTED to prevent dirty reads.</li>
+     * <li>Checks if the account exists in the <code>accounts</code> table and has sufficient funds.</li>
+     * <ul>
+     *     <li>Throws {@link AccountNotFoundException} in case the account does not exist.</li>
+     *     <li>Throws {@link InsufficientFundsException} in case the account does not have enough money on the balance.</li>
+     * </ul>
+     * <li>Updates the account balance in the <code>accounts</code> table by deducting the withdrawal amount.</li>
+     * <li>Inserts a transaction record into the <code>transactions</code> table.</li>
+     * <li>Commits the transaction.</li>
+     * <li>If any step fails, rolls back the transaction. If the rollback fails, throws {@link DaoOperationException}.</li>
+     * </ul>
      *
-     * @param accountId The account to withdraw from
-     * @param amount    The amount to withdraw
-     * @throws AccountNotFoundException   If the account doesn't exist
-     * @throws InsufficientFundsException If the account has insufficient funds
-     * @throws DaoOperationException      If the database operation fails
+     * @param accountId the ID of the account to withdraw from
+     * @param amount    the amount to withdraw
+     * @throws AccountNotFoundException   if the account with the specified ID does not exist
+     * @throws InsufficientFundsException if the account balance is less than the withdrawal amount
+     * @throws DaoOperationException      if there is an error during the transaction or rollback
      */
     @Override
     public void withdrawMoney(int accountId, BigDecimal amount) {
@@ -44,14 +72,35 @@ public class BankingDaoImpl implements BankingDao {
     }
 
     /**
-     * Transfers money between two accounts using REPEATABLE READ isolation.
+     * Transfers money between two accounts using REPEATABLE READ isolation level.
+     * <p>
+     * This method performs the following steps:
+     * <ul>
+     * <li>Validates if the input <code>amount</code> is less than or equal to 0. If true, throws {@link IllegalArgumentException}.</li>
+     * <li>Validate if <code>fromAccountId</code> equals to <code>toAccountId</code>. If true, throws {@link IllegalArgumentException}</li>
+     * <li>Starts a transaction by setting auto-commit to false.</li>
+     * <li>Sets transaction isolation level to REPEATABLE READ to prevent non-repeatable reads.</li>
+     * <li>Checks if the <code>fromAccountId</code> exists in the <code>accounts</code> table and has sufficient funds.</li>
+     * <ul>
+     *     <li>Throws {@link AccountNotFoundException} in case the account does not exist.</li>
+     *     <li>Throws {@link InsufficientFundsException} in case the account does not have enough money on the balance.</li>
+     * </ul>
+     * <li>Checks if the <code>toAccountId</code> exists in the <code>accounts</code> table. If not than throws {@link AccountNotFoundException}</li>
+     * <li>Updates the source account balance in the <code>accounts</code> table by deducting the transfer amount.</li>
+     * <li>Updates the destination account balance in the <code>accounts</code> table by adding the transfer amount.</li>
+     * <li>Inserts transaction records for both accounts into the <code>transaction</code> table. Transaction type
+     *  should be <code>'transfer'</code>, amount should be equal to the input <code>amount</code>,
+     *  description should be <code>'Transfer to/from' + accountId</code></li>
+     * <li>Commits the transaction.</li>
+     * <li>If any step fails, rolls back the transaction. If the rollback fails, throws {@link DaoOperationException}.</li>
+     * </ul>
      *
-     * @param fromAccountId The source account
-     * @param toAccountId   The destination account
-     * @param amount        The amount to transfer
-     * @throws AccountNotFoundException   If either account doesn't exist
-     * @throws InsufficientFundsException If the source account has insufficient funds
-     * @throws DaoOperationException      If the database operation fails
+     * @param fromAccountId the ID of the source account
+     * @param toAccountId   the ID of the destination account
+     * @param amount        the amount to transfer
+     * @throws AccountNotFoundException   if either account does not exist
+     * @throws InsufficientFundsException if the source account has insufficient funds
+     * @throws DaoOperationException      if there is an error during the transaction or rollback
      */
     public void transferMoney(int fromAccountId, int toAccountId, BigDecimal amount) {
         throw new ExerciseNotCompletedException();
@@ -59,13 +108,35 @@ public class BankingDaoImpl implements BankingDao {
 
     /**
      * Processes a loan payment with a savepoint to handle potential failures.
+     * <p>
+     * This method performs the following steps:
+     * <ul>
+     * <li>Validates if the input <code>amount</code> is less than or equal to 0. If true, throws {@link IllegalArgumentException}.</li>
+     * <li>Starts a transaction by setting auto-commit to false.</li>
+     * <li>Sets transaction isolation level to SERIALIZABLE to prevent phantom reads.</li>
+     * <li>Verifies the loan exists in the <code>loans</code> table (throws {@link LoanNotFoundException} if it doesn't)
+     * and gets its current balance.</li>
+     * <li>Throws {@link InvalidPaymentException} if <code>paymentAmount</code> is larger than the loan amount.</li>
+     * <li>Updates the loan balance in the <code>loans</code> table by deducting the payment amount.</li>
+     * <li>Creates Savepoint after updating the loan balance.</li>
+     * <li>Inserts a payment record into the <code>loan_payments</code> table.</li>
+     * <li>Commits the transaction.</li>
+     * <li>If {@link java.sql.SQLException} was caught:
+     * <ul>
+     *     <li>If the savepoint is present, rolls back to this savepoint and retries inserting into the
+     *     <code>loan_payments</code> table. If the retry succeeds, commits the transaction and ends the method execution.</li>
+     *     <li>If the savepoint is not present, rolls back the entire transaction.</li>
+     * </ul>
+     * <li>If any error occurred while handling the exception, tries rolling back the entire transaction. If the rollback
+     * fails, throws {@link DaoOperationException}.</li>
+     * </ul>
      *
-     * @param loanId        The loan to make a payment on
-     * @param paymentAmount The amount to pay
+     * @param loanId        the ID of the loan to make a payment on
+     * @param paymentAmount the amount to pay
      * @param description   the description of the payment
-     * @throws LoanNotFoundException   If the loan doesn't exist
-     * @throws InvalidPaymentException If the payment amount is invalid
-     * @throws DaoOperationException   If the database operation fails
+     * @throws LoanNotFoundException   if the loan with the specified ID does not exist
+     * @throws InvalidPaymentException if the payment amount is invalid (e.g., exceeds loan balance)
+     * @throws DaoOperationException   if there is an error during the transaction or rollback
      */
     public void processLoanPayment(int loanId, BigDecimal paymentAmount, String description) {
         throw new ExerciseNotCompletedException();
