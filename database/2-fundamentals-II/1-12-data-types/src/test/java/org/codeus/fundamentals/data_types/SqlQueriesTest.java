e3c2466379ab76438e21f3631a6b8604f548137f
skravchuk <ext-serhii.krachuk@here.com>
2025-05-21 15:35:09 +0300

    2-fundamentals-II: fixed test ordering, added missing dependencies for module 1-13

--- database/2-fundamentals-II/1-12-data-types/src/test/java/org/codeus/fundamentals/data_types/SqlQueriesTest.java
+++ database/2-fundamentals-II/1-12-data-types/src/test/java/org/codeus/fundamentals/data_types/SqlQueriesTest.java
@@ -1,14 +1,14 @@
 package org.codeus.fundamentals.data_types;
 
 import org.codeus.database.common.EmbeddedPostgreSqlSetup;
-import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.TestClassOrder;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class SqlQueriesTest extends EmbeddedPostgreSqlSetup {
 
   @Order(1)
