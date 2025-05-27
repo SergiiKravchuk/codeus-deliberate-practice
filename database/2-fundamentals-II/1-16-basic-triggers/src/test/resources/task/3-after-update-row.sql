220277bd0f01d555b205ae083a94b150e4eecafa
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-05-27 07:30:30 +0300

    Update 3d task description

--- database/2-fundamentals-II/1-16-basic-triggers/src/test/resources/task/3-after-update-row.sql
+++ database/2-fundamentals-II/1-16-basic-triggers/src/test/resources/task/3-after-update-row.sql
@@ -5,7 +5,7 @@
 -- 1. Create a function named `log_customer_update`.
 --    This function should:
 --    - Insert a record into the audit_logs table when a customer is updated.
---    - Include the customer ID and which field was updated.
+--    - Hint: the same logic as in previous task but triggers after update
 
 
 -- 2. Create a trigger that executes this function AFTER UPDATE on customers.
